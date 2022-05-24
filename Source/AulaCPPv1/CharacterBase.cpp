5fbc14fff77062015f0b6395c8f0125bb9b67281
Yerco <myerco@gmail.com>
2022-05-24 10:53:27 -0400

    feat: Malha adicionada

--- Source/AulaCPPv1/CharacterBase.cpp
+++ Source/AulaCPPv1/CharacterBase.cpp
@@ -9,7 +9,26 @@ ACharacterBase::ACharacterBase()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	/*Construindo a malha na memória 
+	- CreateDefaultSubobject - Cria um componente ou subobjeto, permitindo criar uma classe filho e retornando a classe pai.
+	Os objetos recém-iniciados podem ter alguns de seus valores padrão inicializados, mas o Mesh começará vazio. 
+	Você terá que carregar o malha mais tarde.
+		*/
 	MeshMain = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh Main"));
+
+	/*
+	* ConstructorHelpers - No construtor, inicializamos os componentes e, em seguida, definimos seus valores usando FObjectFinder. 
+	Também configuramos a classe para gerar usando a função StaticClass para recuperar uma instância UStatic* de um tipo de classe.
+	*/
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> SphereVisualAsset(TEXT("/Game/ExampleContent/StarterContent/Shapes/Shape_Sphere.Shape_Sphere"));
+
+	if (SphereVisualAsset.Succeeded()) {
+		MeshMain->SetStaticMesh(SphereVisualAsset.Object);
+		MeshMain->SetRelativeLocation(FVector(0.0f, 0.0f, -100.0f));
+		MeshMain->SetWorldScale3D(FVector(0.8f));
+
+	}
+
 
 	MeshMain->SetupAttachment(RootComponent);
 
--- Source/AulaCPPv1/CharacterBase.h
+++ Source/AulaCPPv1/CharacterBase.h
@@ -15,6 +15,7 @@ public:
 	// Sets default values for this actor's properties
 	ACharacterBase();
 
+	/* Configurando propriedade para adicionar uma Static Mesh */
 	UPROPERTY(VisibleAnywhere)
 		UStaticMeshComponent* MeshMain;
 
